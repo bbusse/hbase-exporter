ce8a99be20786d2cb4a14dcece4149171a8be1cb
Björn Busse <bj.rn@baerlin.eu>
2020-06-25 01:03:59 +0200

    protobuf: Add workaround for imports in generated files

---
+++ protobuf/__init__.py
@@ -0,0 +1,5 @@
+# https://github.com/protocolbuffers/protobuf/issues/1491
+# https://github.com/protocolbuffers/protobuf/issues/1491#issuecomment-547504972
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
