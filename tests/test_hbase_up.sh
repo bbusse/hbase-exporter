7f81df826aa302bdd9bbf5d859347079a1f771a8
Björn Busse <bj.rn@baerlin.eu>
2021-03-03 19:24:18 +0100

    tests: Execute once

--- tests/test_hbase_up.sh
+++ tests/test_hbase_up.sh
@@ -2,28 +2,25 @@
 
 HBASE_HOST="127.0.0.1"
 HBASE_PORT=16200
-ZK_HOST=$HBASE_HOST
+ZK_HOST="$HBASE_HOST"
 ZK_PORT=2181
 HBASE_TIME_STARTUP=8
 HBASE_VERSION="2.4.1"
 
-run_hbase() {
+test_run_hbase() {
     cd "../hbase-${HBASE_VERSION}"
     ./bin/hbase-daemon.sh --config conf start $1
     sleep ${HBASE_TIME_STARTUP}
 }
 
 test_hbase_running() {
-    nc -vnu -w1 $1 $2
+    nc -znu -w1 ${1:-"127.0.0.1"} ${2:-"16200"}
 }
 
 test_hbase_zk_running() {
-    nc -vnu -w1 $1 $2 <<END
+    nc -znu -w1 ${1:-"127.0.0.1"} ${2:-"2181"} <<END
 "ruok"
 END
 }
 
 export JAVA_HOME=${JAVA_HOME:-"/usr/local"}
-run_hbase "master"
-test_hbase_running ${HBASE_HOST} ${HBASE_PORT}
-test_hbase_zk_running ${ZK_HOST} ${ZK_PORT}
