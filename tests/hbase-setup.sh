3105c8ac3f2e231ee75ced9528cab5414b77c8d3
Björn Busse <bj.rn@baerlin.eu>
2021-03-06 00:58:28 +0100

    shellcheck: Fix errors and warnings

--- tests/hbase-setup.sh
+++ tests/hbase-setup.sh
@@ -6,7 +6,6 @@ HBASE_VERSION="2.4.1"
 HBASE_FILE="hbase-${HBASE_VERSION}-bin.tar.gz"
 HBASE_URL="https://downloads.apache.org/hbase/${HBASE_VERSION}/${HBASE_FILE}"
 HBASE_FILE_CKSUM="5afb643c2391461619516624168e042b42a66e25217a3319552264c6af522e3a21a5212bfcba759b7b976794648ef13ee7b5a415f33cdb89bba43d40162aa685"
-HBASE_HOST="127.0.0.1"
 HBASE_CONFIG="hbase/conf/hbase-site.xml"
 HBASE_TEST_SUITE_EXECUTABLE="hbase/bin/hbase"
 
@@ -30,8 +29,8 @@ prepare_hbase() {
                 printf "HBase archive exists\n"
             fi
         else
-            printf "Downloading ${1}\n"
-            curl -LO ${1}
+            printf "Downloading %s\n" "$1"
+            curl -LO "${1}"
         fi
 
         printf "Extracting HBase archive\n"
@@ -42,7 +41,7 @@ prepare_hbase() {
 
 create_config() {
     printf "Writing HBase config\n"
-    cat <<EOF > $2
+    cat <<EOF > "$2"
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
