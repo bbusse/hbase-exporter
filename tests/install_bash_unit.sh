98124f57a8514c1d8d49aa70253ee36d59106931
Björn Busse <bj.rn@baerlin.eu>
2021-03-06 01:21:11 +0100

    shellcheck: Fix errors and warnings

--- tests/install_bash_unit.sh
+++ tests/install_bash_unit.sh
@@ -4,7 +4,7 @@ install() {
     if [ -f "bash_unit" ]; then
         printf "bash_unit test framework exists\n"
     else
-        curl -o /tmp/install.sh -sLO $1
+        curl -o /tmp/install.sh -sLO "$1"
         chmod +x /tmp/install.sh
         /tmp/install.sh
     fi
